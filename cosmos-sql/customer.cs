faef7f05de6792b67df7ae72d340cfacf72b5841
Paras Rajput <paras.code.api@gmail.com>
2020-06-14 23:58:25 +0530

    Add project files.

---
+++ cosmos-sql/customer.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cosmos_sql
+{
+    class customer
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; } 
+
+        [JsonProperty(PropertyName ="customerId")]
+        public string customerId { get; set; }
+        
+        [JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+        
+        [JsonProperty(PropertyName = "city")]
+        public string city { get; set; }
+
+        public customer(string p_id,string p_name,string p_city)
+        {
+            customerId = p_id;
+            name = p_name;
+            city=p_city;
+        }
+    }
+}
